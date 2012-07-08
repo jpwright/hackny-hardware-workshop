663c957aad1d5c99084da61edc21450c42f30110
Jason Wright <jpwright0@gmail.com>
2012-07-08 03:52:50 -0400

    blinky

---
+++ projects/blinky/blinky.ino
@@ -0,0 +1,12 @@
+const int ledPin = 0; //0 on teensy++
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);   // set the LED on
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // set the LED off
+  delay(1000);                  // wait for a second
+}
