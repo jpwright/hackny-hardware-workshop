a3d59144323c104525f63a0f3afb62cc874a6b22
Jason Wright <jpwright0@gmail.com>
2012-07-08 03:32:52 -0400

    superblinky

---
+++ projects/superblinky.ino
@@ -0,0 +1,34 @@
+const int ledPin = 3;    // LED connected to digital PWM pin 3
+const int analogPin = 0; // Potentiometer connected to analog pin 0
+
+int analogVal;
+int delayVal;
+int fadeVal = 0;
+boolean fadeUp = true;
+
+void setup()  { 
+  Serial.begin(38400); // initiate serial connection at 38400 baud
+} 
+
+void loop()  { 
+  analogVal = analogRead(analogPin);
+  
+  delayVal = map(analogVal, 0, 1024, 1, 5);
+  analogWrite(ledPin, fadeVal);           
+  delay(delayVal);                            
+
+  if(fadeUp) {
+    fadeVal++;
+  } else {
+    fadeVal--;
+  }
+  
+  if(fadeVal == 255 || fadeVal == 0) {
+    fadeUp = !fadeUp;
+    Serial.print("analog 0 is: ");
+    Serial.println(analogVal);
+    delay(1); //for stability
+  }
+}
+
+
